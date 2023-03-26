b6a7c99737775916af1ddf02a2d86327c8fe1ab1
Chahid Hadifi <chahidhadifi@gmail.com>
2023-03-26 02:45:17 +0000

    edit the professor navigation bar

--- vuetify/src/App.vue
+++ vuetify/src/App.vue
@@ -8,7 +8,8 @@
 </template>
 
 <script>
-import Navbar from "@/components/Navbar";
+// import Navbar from "@/components/Navbar";
+import Navbar from "@/components/NavbarProfesseur.vue";
 //import Footer from '@/components/Footer'
 export default {
     name: "App",
---
+++ vuetify/src/components/NavbarProfesseur.vue
@@ -0,0 +1,82 @@
+<template>
+    <nav>
+        <v-card>
+            <v-navigation-drawer permanent dark app class="blue darken-3 py-0">
+                <v-list-item class="px-2">
+                    <v-list-item-avatar>
+                        <v-img src="@/assets/user.png"></v-img>
+                    </v-list-item-avatar>
+                    <v-list-item-title>{{ username }}</v-list-item-title>
+                </v-list-item>
+
+                <v-divider></v-divider>
+
+                <v-list nav dense>
+                    <v-list-item router to="/">
+                        <v-list-item-action>
+                            <v-icon>mdi-view-dashboard</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>Dashboard</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item router to="/examens">
+                        <v-list-item-action>
+                            <v-icon>mdi mdi-note-text</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>Examens</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item router to="/questions">
+                        <v-list-item-action>
+                            <v-icon>mdi mdi-tooltip-question</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>Questions</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list-item router to="/propositions">
+                        <v-list-item-action>
+                            <v-icon>mdi mdi-note-check</v-icon>
+                        </v-list-item-action>
+                        <v-list-item-content>
+                            <v-list-item-title>Propositions</v-list-item-title>
+                        </v-list-item-content>
+                    </v-list-item>
+                    <v-list style="position: absolute; bottom: 0" nav dense>
+                        <v-list-item router to="/auth">
+                            <v-icon large @click="logout()"
+                                >mdi mdi-logout-variant</v-icon
+                            >
+                        </v-list-item>
+                    </v-list>
+                </v-list>
+            </v-navigation-drawer>
+        </v-card>
+    </nav>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            items: [
+                { title: "Home", icon: "mdi-home-city" },
+                { title: "My Account", icon: "mdi-account" },
+                { title: "Users", icon: "mdi-account-group-outline" },
+            ],
+            mini: true,
+        };
+    },
+    computed: {
+        username() {
+            return localStorage.getItem("name");
+        },
+    },
+    methods: {
+        logout() {
+            localStorage.clear();
+        },
+    },
+};
+</script>
